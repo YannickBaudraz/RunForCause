92d45017240a27ca16e97b9c4f496cc8c6af5cc9
Yannick Clifford <yannickbaudrazdev@gmail.com>
2022-05-20 08:55:03 +0200

    Login to the api with credentials

--- componnents/AuthForm.tsx
+++ componnents/AuthForm.tsx
@@ -1,30 +1,47 @@
 import {StyleSheet, Text, TextInput, View} from "react-native";
 import {Button} from "react-native-elements";
+import {useState} from "react";
+import axios from "axios";
 
 export function AuthForm() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
   return (
       <View style={styles.loginScreenContainer}>
         <View style={styles.loginFormView}>
           <Text style={styles.logoText}>LOGIN</Text>
           <TextInput
-              placeholder="Username"
+              placeholder="Email"
               placeholderTextColor="#c4c3cb"
               style={styles.loginFormTextInput}
+              onChangeText={setEmail}
           />
           <TextInput
               placeholder="Password"
               placeholderTextColor="#c4c3cb"
               secureTextEntry={true}
               style={styles.loginFormTextInput}
+              onChangeText={setPassword}
           />
           <Button
               buttonStyle={styles.loginButton}
               title="Login"
+              onPress={() => onPress(email, password)}
           />
         </View>
       </View>
   )
+}
+
+function onPress(email: string, password: string) {
+  axios.post(`https://mockapi.mycpnv.ch/api/rfc/mytoken`, {
+    username: email,
+    password: password
+  }).then(res => {
+    console.log(res);
+    alert("Login successful, token: " + res.data);
+  })
 }
 
 const styles = StyleSheet.create({
